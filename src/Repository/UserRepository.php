7fa6e4920d11d090a8e6f5c898973074ca9d37db
Rivière Mickael <mickaelr20@gmail.com>
2022-06-28 16:42:29 +0400

    Ajout parametres utilisateurs
    modification photo de profile, pseudo et email
    Modification mot de passe

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -56,28 +56,15 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->add($user, true);
     }
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * @return User Returns a user with the username in param or null
+     */
+    public function get($username): User|null
+    {
+        return $this->createQueryBuilder('users')
+        ->andWhere('users.username = :username')
+        ->setParameter('username', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
