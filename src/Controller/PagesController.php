04e40f767fd54b3b45ca7a3865f7f36805a70411
Rivière Mickael <mickaelr20@gmail.com>
2022-07-18 16:27:39 +0400

    Liste des pages par 2

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TrickRepository;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class PagesController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(TrickRepository $trickRepository): Response
     {
-        $tricks = $trickRepository->listPage(0);
+        $tricks = $trickRepository->listPage(0, 2);
         return $this->render('pages/home.html.twig', [
             'controller_name' => 'PagesController',
             'tricks' => $tricks,
